fc755faabe4cdea204d09cfe73bf1f46cc7f7c9b
ione gulkin <isgulkin@edu.hse.ru>
2023-06-02 23:46:47 +0300

    final changes to 2cw

--- information_theory/controlworks/controlwork2/second.py
+++ information_theory/controlworks/controlwork2/second.py
@@ -27,7 +27,10 @@ def evaluate_polynomial(polynomial: galois.Poly,
 def find_sx(polynomial: galois.Poly,
             field_elements: list) -> list[Array | Poly]:
     polynomial_values = []
+    count = 0
     for element in field_elements:
+        if count > 3:
+            break
         polynomial_value = polynomial(element)
         polynomial_degrees = polynomial.degrees
         polynomial_terms = []
@@ -37,14 +40,13 @@ def find_sx(polynomial: galois.Poly,
                 polynomial_terms.append(coefficient * element ** polynomial_degrees[i])
 
         print('S(' + str(element) + ") = v'(" + str(element) + ") =",
-              ' + '.join(map(str, polynomial_terms)), '=', polynomial_value, end=' ')
+              ' + '.join(map(str, polynomial_terms)), '=', end=' ')
 
         print(*['[' + power_to_int.get(i.__str__(), '0') + ']'
                 for i in polynomial_terms], sep=" + ", end=" = " + str(polynomial_value) + "\n")
 
         polynomial_values.append(polynomial_value)
-        if polynomial_value == 0:
-            break
+        count += 1
     return polynomial_values
 
 
@@ -73,10 +75,24 @@ def find_errors(polynomial: galois.Poly,
 
 prime, power = 3, 2
 GaloisField = galois.GF(prime ** power, irreducible_poly='x^2+x+2', repr='power')
+rs = galois.ReedSolomon(n=8, k=4, field=GaloisField)
 
-power_reprs = [i.__str__() for i in GaloisField.elements][1:]
+print('--------------------------------------------------------------------')
 
-int_reprs = [np.base_repr(i, prime).zfill(power) for i in GaloisField.elements][1:]
+print('Нахождение g(x):')
+g = galois.Poly([1], field=GaloisField)
+
+for i in rs.roots:
+    el = galois.Poly([1, i], field=GaloisField)
+    print(f'({g})', '*', f'({el})', end='= ')
+    g *= el
+    print(g)
+
+print('--------------------------------------------------------------------')
+
+power_reprs = [i.__str__() for i in GaloisField.elements][:]
+
+int_reprs = [np.base_repr(i, prime).zfill(power) for i in GaloisField.elements][:]
 
 power_to_int, int_to_power = {}, {}
 for i in range(len(power_reprs)):
@@ -91,17 +107,29 @@ elements = sorted(GaloisField.elements, key=lambda x: x.__str__())
 
 print('--------------------------------------------------------------------')
 
-print('Нахождение вектора u путём подставления в а(х) всех элементов поля\n')
+print("Введите элементы вектора через пробел (от 1 до 4 элементов):")
+print('Есть вектор [1, 1, α, α^2] -> вводить элементы поля в виде: 2,1 1,0 0,1 0,1')
+input_a = [list(map(int, i.split(','))) for i in input().split()]
 
-q, w = GaloisField.Vector([1, 0]), GaloisField.Vector([2, 1])
-a = [1, 1, q, w][::-1]
-a = galois.Poly(a, field=GaloisField)
+input_a = [GaloisField.Vector(i) for i in input_a]
+
+a = galois.Poly(input_a, field=GaloisField)
+print('Представление вектора а в виде полинома: a(x)=', a)
+
+print('--------------------------------------------------------------------')
+
+print('Нахождение вектора u путём подставления в а(х) всех элементов поля\n')
 u = evaluate_polynomial(a, elements[1:])
 print('\nВектор u:', *u)
 
-a_5, a_7 = GaloisField.Vector([2, 0]), GaloisField.Vector([1, 1])
-v_ = [0, 0, 0, 0, a_5, 0, a_7, 1][::-1]
-v_ = galois.Poly(v_, field=GaloisField)
+print("Введите элементы вектора через пробел (от 1 до 4 элементов):")
+print('Есть вектор [0, α^5, α, α^5, α^3, α^5, α^4, 1]')
+print('-> вводить элементы поля в виде: 2,0 0,0 1,0 2,0 2,2 2,0 0,2 0,1')
+input_v = [list(map(int, i.split(','))) for i in input().split()]
+
+input_v = [GaloisField.Vector(i) for i in input_v]
+
+v_ = galois.Poly(input_v, field=GaloisField)
 
 print("Представление v' в виде полинома: v'(x) =", v_)
 
@@ -117,11 +145,23 @@ print('--------------------------------------------------------------------')
 
 q = galois.Poly([1, 0, 0, 0, 0], field=GaloisField)
 q_0 = divmod(q, sz)[0]
+print(q)
+coefs = q_0.coefficients()
+
+for i in range(len(coefs)):
+    mul = [0 for _ in coefs]
+    mul[i] = coefs[i]
+    print('\t' * i + '-')
+    print('\t' * i + f'{sz * galois.Poly(mul, field=GaloisField)}')
+    q -= sz * galois.Poly(mul, field=GaloisField)
+    print('\t' * i + '----------------------')
+    print('\t' * (i + 1) + f'{q}')
+
 print('Частное получившееся от деления двух многочленов:')
-print('q_0 = (' + str(q).replace('x', 'z') + ') / [' + str(sz).replace('x', 'z') + '] = ' + str(q_0).replace('x', 'z'))
+print('q_0 = ' + str(q_0).replace('x', 'z'))
 
 free_term = q_0.coeffs[-1]
-print('Свободный член: q_0 =', free_term)
+print('Свободный член: q =', free_term)
 
 print('--------------------------------------------------------------------')
 
